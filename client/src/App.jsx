4b6d5ba83fd301ce81103af704cb46c29dce45e2
rajeev-koushik <rajeevkoushikyg@gmail.com>
2023-05-19 22:02:49 +0530

    Created Login and Register components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import IndexPage from './pages/IndexPage'
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/loginPage";
+import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
