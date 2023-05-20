201cc10169a8932e7b78331c6591f92dbf428865
rajeev-koushik <rajeevkoushikyg@gmail.com>
2023-05-20 12:29:32 +0530

    Registration and Login working
    Connected to a MongoDB Atlas instance to store user registration data

---
+++ api/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
+require("dotenv").config();
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "aefa8i94h3yb87gbr89ghba";
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userDoc = await User.findOne({
+      email,
+    });
+    if (userDoc) {
+      const passOk = bcrypt.compareSync(password, userDoc.password);
+      if (passOk) {
+        jwt.sign(
+          { email: userDoc.email, id: userDoc._id },
+          jwtSecret,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res.status(200).cookie("token", token).json("Pass OK");
+          }
+        );
+      } else {
+        res.status(422).json("Pass not OK");
+      }
+    } else {
+      res.status(404).json("Not found");
+    }
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.listen(4000);
