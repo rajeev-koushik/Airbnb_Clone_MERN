5352dbc5d92ef21df48e980a2a572774cac29c0f
rajeev-koushik <rajeevkoushikyg@gmail.com>
2023-05-27 15:49:51 +0530

    Account Page ready

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
   const { user } = useContext(UserContext);
   return (
     <header className="flex justify-between">
-      <a href="/" className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-1">
         <img src="/Airbnb-logo.png" className="w-20 h-11" alt="Airbnb logo" />
-      </a>
+      </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300"></div>
@@ -33,7 +33,7 @@ export default function Header() {
         </button>
       </div>
       <Link
-        to={"/login"}
+        to={user ? "/account" : "/login"}
         className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
       >
         <svg
